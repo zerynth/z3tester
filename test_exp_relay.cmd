5c42cf095a96cc78bf912ca237d07e30d4455cc6
m.cipriani <m.cipriani@zerynth.com>
2021-03-25 13:30:09 +0100

    added ota and sdkconfig binaries

--- test_exp_relay.cmd
+++ test_exp_relay.cmd
@@ -4,7 +4,7 @@ set /p myvar= < tmpFile
 del tmpFile 
 echo %myvar%
 "ztool\sys\python\python.exe" -I "ztool/sys/esptool/esp32/esptool.py" --chip esp32 --port %myvar% --baud 460800 erase_region 0x350000 0x1000 
-"ztool\sys\python\python.exe" -I "ztool/sys/esptool/esp32/esptool.py" --chip esp32 --port %myvar% --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 "ztool/dist/devices/db_zm1/test/exp-relay/bootloader.bin" 0x10000 "ztool/dist/devices/db_zm1/test/exp-relay/zerynth.bin" 0x9000 "ztool/dist/devices/db_zm1/test/exp-relay/partition-table.bin"
+"ztool\sys\python\python.exe" -I "ztool/sys/esptool/esp32/esptool.py" --chip esp32 --port %myvar% --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 "ztool/dist/devices/db_zm1/test/exp-relay/bootloader.bin" 0x10000 "ztool/dist/devices/db_zm1/test/exp-relay/zerynth.bin" 0x9000 "ztool/dist/devices/db_zm1/test/exp-relay/partition-table.bin" 0x310000 "ztool/dist/devices/db_zm1/test/db-zm1/otalog.bin" 0x320000 "ztool/dist/devices/db_zm1/test/db-zm1/firmware.bin"
 echo "-------------------------------------"
 echo "             TEST EXP-RELAY          "
 echo "-------------------------------------"
