3cb1cbd56765264e5423c4c55b6835b92ea66ac8
eliag <e.guglielmin@zerynth.com>
2022-12-21 14:37:23 +0100

    fixed format
    Signed-off-by: eliag <e.guglielmin@zerynth.com>

--- test_4zerobox_v10_ram.cmd
+++ test_4zerobox_v10_ram.cmd
@@ -5,13 +5,13 @@ del tmpFile
 echo %myvar%
 "ztool\sys\python_new\python.exe" -I "ztool/sys/esptool/esp32/esptool.py" --chip esp32 --port %myvar% --baud 460800 erase_region 0x350000 0x1000 
 "ztool\sys\python_new\python.exe" -I "ztool/sys/esptool/esp32/esptool.py" --chip esp32 --port %myvar% --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 "ztool/dist/devices/db_zm1/test/psram-v10/bootloader.bin" 0x10000 "ztool/dist/devices/db_zm1/test/psram-v10/zerynth.bin" 0x9000 "ztool/dist/devices/db_zm1/test/psram-v10/partition-table.bin" 0x910000 "ztool/dist/devices/db_zm1/test/psram-v10/otalog.bin" 0x210000 "ztool/dist/devices/db_zm1/test/psram-v10/firmware.bin" 
-echo "-------------------------------------"
+echo "------------------------------------------------"
 echo "            TEST 4ZeroBoxV10 PSRAM              "
-echo "-------------------------------------"
+echo "------------------------------------------------"
 echo "Before starting the test:"
 echo "  --> Use USB-C cable to program the board and check messages"
 echo "----------------------------------------------------------------------------------"
-echo "#####################################################"
+echo "################################################################"
 echo "###  Press Reset Button to start the 4ZeroBoxV10 PSRAM Test  ###"
-echo "#####################################################"
+echo "################################################################"
 "ztool\sys\python\python.exe" -I "ztool/dist/ztc/ztc.py" device open_raw %myvar%
